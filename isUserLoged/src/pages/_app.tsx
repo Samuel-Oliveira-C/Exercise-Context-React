864f5bdb093e7490c7fa0ba40ab73e0816a90609
Samuel Oliveira <samuel.oleiv@gmail.com>
2024-10-11 15:03:39 -0300

    Criado o projeto no Next

---
+++ isUserLoged/src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import "@/styles/globals.css";
+import { SwitchUser } from "@/components/IsLoged";
+import { ContextComponent } from "@/contexts/contextUser";
+
+export default function App() {
+  return (
+    <>
+      <header>
+        <h1>Titulo da pagina</h1>
+
+      </header>
+    </>
+  );
+}
